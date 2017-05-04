e89816f388548f6dcc6da3a16c37997b37ebd1fe
Yercko <yercko@gmail.com>
2017-05-04 17:53:48 +0200

    Initial commit

---
+++ app/src/main/java/es/orcelis/orcelis/MainActivity.java
@@ -0,0 +1,44 @@
+package es.orcelis.orcelis;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_parcelas:
+                    mTextMessage.setText(R.string.title_parcelas);
+                    return true;
+                case R.id.navigation_informes:
+                    mTextMessage.setText(R.string.title_parcelas);
+                    return true;
+                case R.id.navigation_configuracion:
+                    mTextMessage.setText(R.string.title_configuracion);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
